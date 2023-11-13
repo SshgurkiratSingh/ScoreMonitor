b029d8656acb37b1ba883ec42acb7c989b245e2f
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-11-13 08:12:07 +0530

    feat

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    generateEtags: false,
+    // Add more configurations here as needed
+  }
+  
 
 module.exports = nextConfig
